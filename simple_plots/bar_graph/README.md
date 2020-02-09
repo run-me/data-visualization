2314a5e0bd5b78eabbbc5221c48f6621e884f2fb
mash-tensor <wys925@my.utsa.edu>
2020-02-09 11:11:53 -0600

    simple bar plot added with incomplete readme

--- simple_plots/bar_graph/README.md
+++ simple_plots/bar_graph/README.md
@@ -1,1 +1,14 @@
-# Bar graph using housing price
+# Plotting Basics
+
+## Whats the aim of plotting data?
+
+Data is plotted with an aim that it will give us visual 
+insight of the data by looking at a single picture.
+
+### Lets take an example:
+
+Consider the following, you are given an data collected from 
+a lets say the humble temperature sensor. 
+
+
+of data which has 1000 rows 
--- simple_plots/bar_graph/plot_bar.py
+++ simple_plots/bar_graph/plot_bar.py
@@ -2,13 +2,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+style = plt.style.use('ggplot')
+
+plt.style.use(style)
 data_path = "/home/mash-tensor/universe/dev/github-runme/data-visualization/data/housing_prices/train.csv"
 data = pd.read_csv(data_path)
 
 data = data.sort_values(by=['YearBuilt'])
 
-year_built = data['YearBuilt'][200:]
-quality = data['SalePrice'][200:]
+year_built = data['YearBuilt'][200:1000]
+sale_price = data['SalePrice'][200:1000]
+area = data["LotArea"][200:1000]
 
-plt.plot(year_built, quality)
+plt.plot(year_built, sale_price, label="Sale Price", color='b', linestyle='--', marker='+')
+plt.plot(year_built, area, label="Sq foot area", color='r', linestyle='-', marker='o')
+
+plt.xlabel("year built -->")
+plt.ylabel("price in USD -->")
+
+plt.legend()
+
+plt.title('House price in USD by built year style of the plot {}'.format(style))
 plt.show()
